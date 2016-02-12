6d437a17a0ac1cd93071771562cf85a56eb5909d
Tim <timshingyu@gmail.com>
2016-02-12 15:14:47 +0800

    Modified request spec to pass hash instead of json

--- spec/requests/api/v1/leads_spec.rb
+++ spec/requests/api/v1/leads_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "POST /api/v1/leads" do
   it "create the lead" do
     lead_params = attributes_for(:lead)
 
-    post "/api/v1/leads", lead: lead_params
+    post "/api/v1/leads", lead_params
 
     expect(response.status).to eq 201
     expect(Lead.last.company).to eq lead_params[:company]
