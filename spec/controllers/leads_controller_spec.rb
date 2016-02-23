87006a1d8b775aff6f383e19531977200ae76cbb
Tim <timshingyu@gmail.com>
2016-02-24 00:56:13 +0800

    Added mocks for leads controller spec

--- spec/controllers/leads_controller_spec.rb
+++ spec/controllers/leads_controller_spec.rb
@@ -22,5 +22,15 @@ RSpec.describe LeadsController do
         expect(response).to render_template :new
       end
     end
+
+    context "when lead is valid" do
+      it "sends email to sale team" do
+        valid_lead = double(save: true)
+        allow(Lead).to receive(:new).and_return(valid_lead)
+        expect(LeadMailer).to receive(:new_lead).with(valid_lead)
+
+        post :create, lead: { attribute: "value" }
+      end
+    end
   end
 end
